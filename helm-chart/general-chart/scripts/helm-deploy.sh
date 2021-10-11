f3d8316a5b1ee3ef7e3ba86469018a284fd86de2
Apichaya Sakchaichanchol <apichayasakchaichanchol@Apichayas-MacBook-Pro.local>
2021-10-11 09:24:46 +0700

    demo nginx

--- helm-chart/general-chart/scripts/helm-deploy.sh
+++ helm-chart/general-chart/scripts/helm-deploy.sh
@@ -5,8 +5,6 @@
     To update project with helm upgrade command
   .POSITION_PARAMETER EKS_CLUSTER_NAME
     EKS cluster name
-  .POSITION_PARAMETER BU
-    Bussiness unit
   .POSITION_PARAMETER PROJECT
     Project name
   .POSITION_PARAMETER APP
@@ -21,8 +19,9 @@
     Need to do these step before run the script
       - Set AWS credentials
       - Provide environment config (and secret)
+      - Install realpath for mac (brew install coreutils)
   .EXAMPLE
-    ./helm-upgrade.sh common-nonprod common price-middleware price-product-middleware sit latest
+    ./helm-deploy.sh docker-desktop project app dev latest
 DOC
 
 # e: immediately exit if error
@@ -33,19 +32,20 @@ set -euo pipefail
 HELMDIR="$(dirname $(realpath "$0"))/.."
 
 EKS_CLUSTER_NAME=$1
-BU=$2
-PROJECT=$3
-APP=$4
-ENV=$5
-VERSION=$6
-if [ $# -ge 7  ]; then
-  OPTION="${@:7}"
+PROJECT=$2
+APP=$3
+ENV=$4
+VERSION=$5
+if [ $# -ge 6  ]; then
+  OPTION="${@:6}"
 else
   OPTION=""
 fi
 echo $OPTION
 
-aws eks --region "ap-southeast-1" update-kubeconfig --name ${EKS_CLUSTER_NAME}
+# aws eks --region "ap-southeast-1" update-kubeconfig --name ${EKS_CLUSTER_NAME}
+kubectl config use-context $EKS_CLUSTER_NAME
+kubectl config current-context
 
 NAMESPACE=${PROJECT}-${ENV}
 RELEASE_NAME=${APP}-${ENV}
@@ -58,9 +58,13 @@ helm upgrade --install $RELEASE_NAME --namespace $NAMESPACE \
 -f $HELMDIR/values/$APP/values.yaml \
 -f $HELMDIR/values/$APP/$ENV.yaml \
 --set env=$ENV \
---set bu=$BU \
 --set project=$PROJECT \
 --set app=$APP \
---set version=$VERSION $OPTION $HELMDIR
+--set container.version=$VERSION \
+$OPTION $HELMDIR
 
+echo "==== Resources ==="
 helm get manifest $RELEASE_NAME --namespace $NAMESPACE | kubectl get -f -
+
+echo "==== Running images ==="
+kubectl get pods --namespace $NAMESPACE -o jsonpath="{.items[*].spec.containers[*].image}"
